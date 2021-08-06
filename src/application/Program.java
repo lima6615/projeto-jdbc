ebd31bf9254bf734263057e38bfa8c80162a372f
lima6615 <leo-queiroz1@outlook.com>
2021-08-06 12:23:14 -0300

    Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,21 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		Department obj = new Department(1,"Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21,"Leoanrdo","leo@gmail.com" ,sdf.parse("15/03/1999"),3000.0 , obj);
+		System.out.println(seller);
 
 	}
 
