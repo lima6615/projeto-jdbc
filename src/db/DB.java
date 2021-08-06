e79e92fab54cccbfd80cb863ec21fe63a1cc8e34
lima6615 <leo-queiroz1@outlook.com>
2021-08-06 11:21:36 -0300

    Criando um novo Projeto

---
+++ src/db/DB.java
@@ -0,0 +1,77 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+
+	private static  Connection conn = null;
+	
+	//Conectando ao banco de dados
+	public static Connection getConnection(){
+		if(conn == null) {
+			try{
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url,props);
+			}catch(SQLException e){
+				throw new DbException(e.getMessage());
+			}
+		}
+			return conn;
+	}
+	
+	//fechando a conexão do bando de dados 
+	public static void closeConnection() {
+		if(conn != null) {
+			try {
+			conn.close();
+			}catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	
+	//carregando as propriedades auxiliares no arquivo "db.properties"
+	private static Properties loadProperties() {
+		try(FileInputStream fs = new FileInputStream("db.properties")){
+			Properties props = new Properties();
+			props.load(fs);//fazendo a leitura do arquivo db.properties
+			return props;
+		}catch(IOException e) {
+			throw new DbException (e.getMessage());
+		}
+	}
+	
+	
+	
+	//metodos para fechar os Objetos 
+	public static void closeStatement(Statement st) {
+		if(st != null) {
+			try {
+				st.close();
+			}catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	//metodos para fechar os Objetos 
+	public static void closeResultSet(ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			}catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+}
